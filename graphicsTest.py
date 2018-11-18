5c4d34215d13bc1c062c87bae5d9e1d458eb34c5
MattKey <m4ttk3y@gmail.com>
2018-11-17 16:30:28 -0800

    begin keymapping & small tweaks
    I added the ability to register key inputs, as well as fixed a few math issues (the road lines used to scale to the edges of the window, and now they scale to the edges of the road).
    
    Press q to toggle the grid lines, and press esc to quit

--- graphicsTest.py
+++ graphicsTest.py
@@ -1,4 +1,5 @@
-#skippity pop
+from pynput import keyboard
+from pynput.keyboard import Key, Listener
 from graphics import *
 
 WIDTH, HEIGHT = 1200.0, 700.0
@@ -6,45 +7,79 @@ WIDTH, HEIGHT = 1200.0, 700.0
 #grab speed & accel from outside file
 
 speed = 2.0
-acceleration = 0.005
-
+acceleration = 0.01
+win = GraphWin('Speedy Wheely Automobiley', WIDTH, HEIGHT)
 points = 0
 p2Win = 100000
+done = False
+qp = False
+drawn = False
 
-lines = []
+grid = []
+listener = None
 
 def main():
-    roadBuff = 50.0
+    global listener
+    listener = Listener(
+            on_press=on_press)
+    listener.start()
+    roadBuff = 75.0
     global speed
-    win = GraphWin('Speedy Wheely Automobiley', WIDTH, HEIGHT)
+    global lines
+    global done
+    global points
+    global win
+    global qp
+    global drawn
     win.setBackground('burlywood')
 
     drawRoad(win, roadBuff)
-    for i in range(7):
-        line = Line(Point(i*(WIDTH/7), 0), Point(i*(WIDTH/7), HEIGHT))
-        line.draw(win)
-
-    done = False
-    lines = drawLines(2, win, roadBuff)
-    global points
+    for i in range(6):
+        line = Line(Point((i+1)*(WIDTH/7), 0), Point((i+1)*(WIDTH/7), HEIGHT))
+        grid.append(line)
+        #line.draw(win)
+    lines = drawLines(4, win, roadBuff)
+    
     while not done:
+        if qp:
+            if not drawn:
+                for g in grid:
+                    g.draw(win)
+            else:
+                for g in grid:
+                    g.undraw()
+            qp = False
+            drawn = not drawn
         for i in lines:
             rmove(i, -speed, 0)
-        done = win.checkMouse()
-        #speed += acceleration
+        speed += acceleration
         points += 1
         if points >= p2Win:
             done = True
             print("you win!")
-        print(points)
+        #print(points)
 
+    end_game()
+
+def end_game():
+    global listener
+    global win
+    listener.stop()
     win.close()
 
+def on_press(key):
+    global done
+    global drawn
+    global qp
+    if key == Key.esc:
+        done = True
+    if key.char == 'q': #press q to quit the game
+        qp = True
+    #print(type(key))
+
 def rmove(rect, xm, ym):
-    rWidth = rect.getP2().getX() - rect.getP1().getX()
-    rCenter = rect.getP1().getX() + rWidth
-    if(rect.getP2().getX() <= -rWidth):
-        rect.move(WIDTH+(rWidth*2), ym)
+    if(rect.getP2().getX() <= -50):
+        rect.move(WIDTH+100, ym)
     else:
         rect.move(xm, ym)
 
@@ -63,13 +98,16 @@ def drawRoad(win, roadBuff):
 
 def drawLines(numLines, win, buff):
     line = []
-    numTix = int((WIDTH / 2)/buff)
+    #numTix = int((WIDTH / 2)/buff)
+    numTix = int((WIDTH/2)/50.0)
     
-    roadWidBuff = HEIGHT/(numLines+1)
+    #roadWidBuff = (HEIGHT-(buff*2))/(numLines+1)
+    roadWidBuff = (HEIGHT-(buff*2))/(numLines+1)
 
     for k in range(numLines):
         for i in range(numTix+1):
-            yr = make_rect((i*(buff*2)+(buff), (k+1)*roadWidBuff),(buff, buff/(2*numLines)))
+            #yr = make_rect((i*(buff*2)+(buff), buff+(k+1)*roadWidBuff),(buff, buff/(2*numLines)))
+            yr = make_rect((i*(100.0)+(50.0), buff+(k+1)*roadWidBuff),(50.0, 50.0/(2*numLines)))
             yrect = Rectangle(yr[0], yr[1])
             yrect.setOutline("black")
             yrect.setFill("yellow")

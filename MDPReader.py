d5a29cf02d7e259321f5daf48fcce02b53c07523
aaronm-2112 <aaron.marroquin96@gmail.com>
2018-11-26 21:49:12 -0800

    The MDPReader class is setup with basic functionalities

--- MDPReader.py
+++ MDPReader.py
@@ -1,16 +1,20 @@
 #Purpose: This program will be responsible for reading in the MDP and returning information back to our agent for use within our AI algorithms
 class MDPReader:
     
+    #e is empty , b is bad car, p is player, oil is o 
     
+    #tells us where the bad cars are located on the board. Will work for when there is one bad car, or more.
+    #The returned list is list of tuples, where each tuple is an (x,y) coordinate. 
     def getListOfCarCoordinates(self, MDP):
         carCoordinateArray = []
         for i in range(len(MDP)):
             for j in range(len(MDP[i])):
-                if MDP[i][j] == 'c' :
+                if MDP[i][j] == 'b' :
                     carCoordinateArray.append((i,j)) 
         return carCoordinateArray     
             
-    
+    #this tells us where the oil spills are within the MDP. Will work for one oil spill, or more.
+    #The returned list is list of tuples, where each tuple is an (x,y) coordinate. 
     def getListOfOilCoordinates (self, MDP):
         oilCoordinateArray = [] 
         for i in range(len(MDP)):
@@ -19,16 +23,57 @@ class MDPReader:
                     oilCoordinateArray.append((i,j)) 
         return oilCoordinateArray 
 
-            
-         
- 
-columns = 7
-numLanes = 2 
-MDP = [['c' for x in range(columns)] for y in range (numLanes)] 
-MDP[0][1] = 'o' 
-a = MDPReader()  
+    #this tells us where our agent currently is within the MDP.
+    #The returned list is list of tuples, where each tuple is an (x,y) coordinate. 
+    def getAgentCoordinates(self, MDP):
+        playerCoordinates = (0,0) 
+        for i in range(len(MDP)):
+            for j in range(len(MDP[i])):
+                if MDP[i][j] == 'p':
+                    playerCoordinates = (i,j) 
+        return playerCoordinates 
+    
+    #this tells us all of the moves our agents can make. 
+    #The returned list is list of tuples, where each tuple is an (x,y) coordinate. 
+    def getLegalActions (self, MDP): 
+        currentPlayerPosition = self.getAgentCoordinates(MDP)
+        #get length of the row and columns
+        MDPRows = len(MDP) 
+        MDPColumns = len(MDP[0])
+        #break up the coordinates 
+        row = currentPlayerPosition[0] 
+        column = currentPlayerPosition[1] 
+        #create a list to hold all of the agent's moves
+        legalActions = [] 
+        
+        #want to know, can we move up or down, which is i, or left and right, which is denoted by j 
+        #first check to see if i+1 and/or i-1 are available moves 
+        if (row+1 < MDPRows and MDPRows > 1) :
+            legalActions.append((row+1, column)) 
+        if (row-1 >= 0 and MDPRows > 1):
+            legalActions.append((row-1, column)) 
+        
+        #then, check to see if j+1 or j-1 are available moves 
+        if (column+1 < MDPColumns and MDPColumns > 1) :
+            legalActions.append((row, column+1)) 
+        if (column-1 >= 0 and MDPColumns > 1) :
+            legalActions.append((row, column-1)) 
+        
+        return legalActions 
+        
+    #pass in two sets of coordinates, and get the manhattan distance between them 
+    def manhattanDistance (self, xy1, xy2):
+        "Returns the Manhattan distance between points xy1 and xy2"
+        return abs( xy1[0] - xy2[0] ) + abs( xy1[1] - xy2[1] )
+
+#some things used for testing. Commented out right now, but if you want to test something, you can uncomment and do it here :)         
+#columns = 7
+#numLanes = 2 
+#MDP = [['c' for x in range(columns)] for y in range (numLanes)] 
+#MDP[0][0] = 'p' 
+#a = MDPReader()  
 
 
-newArray = [] 
-newArray = a.getListOfOilCoordinates(MDP) 
-print (newArray)
+#newArray = [] 
+#newArray = a.getLegalActions(MDP) 
+#print (newArray)
---
+++ graphicsTest.py
@@ -0,0 +1,442 @@
+#import libraries
+import sys
+import random
+from pynput import keyboard
+from pynput.keyboard import Key, Listener
+from graphics import *
+from PIL import Image as img
+
+#global controller variables
+numLanes = 5    #reccomended max: 7
+numCars = 1     #max 2, min 1
+numOil = 1      #max 2, min 1
+
+#set global variables
+WIDTH, HEIGHT = 1200.0, 700.0       #window size (leave alone)
+speed = 2.0                         #"car speed" (line speed)
+acceleration = 0.01                 #"car acceleration" (line accel.)
+win = GraphWin('Speedy Wheely Automobiley', WIDTH, HEIGHT)  #graphics window
+points = 0              #points
+p2Win = 3000            #points to win
+done = False            #done flag
+qp = False              #"q pressed" flag for grid toggle
+drawn = False           #drawn flag for grid toggle
+grid = []               #grid array (holds type line from graphics)
+gridcoords = []         #array for grid line x-coordinates
+MDP = []                #array for MDP
+listener = None         #keyboard listener, to be created in main()
+playCar = None          #player car object
+carX = 0                #car X-coordinate
+carY = 0                #car Y-coordinate
+xcoords = []            #board x-coordinates
+ycoords = []            #board y-coordinates
+move = False            #player movement flag
+oil, oilX, oilY = None, 0, 0
+oil2, oil2X, oil2Y = None, 0, 0
+badCar, bcarX, bcarY, bcarSp = None, 0, 0, 0
+badCar2, bcar2X, bcar2Y, bcar2Sp = None, 0, 0, 0
+
+def main():
+    #grab global variables
+    global listener
+    listener = Listener(
+            on_press=on_press)
+    listener.start()
+    roadBuff = 75.0
+    global speed
+    global lines
+    global done
+    global points
+    global win
+    global qp
+    global drawn
+    global grid, gridcoords
+    global MDP
+    global playCar, carX, carY
+    global xcoords
+    global ycoords
+    global move
+    global oil, oilX, oilY
+    global badCar, bcarX, bcarY, bcarSp
+    global numLanes
+    global numCars, numOil
+    if numCars > 1:
+        global badCar2, bcar2X, bcar2Y, bcar2Sp
+    if numOil > 1:
+        global oil2, oil2X, oil2Y
+
+    #set background (dirt road)
+    win.setBackground('burlywood')
+
+    #draw grey road
+    drawRoad(win, roadBuff)
+    #create lines for grid, but don't draw them yet
+    for i in range(8):
+        line = Line(Point(int((i)*(WIDTH/7)), 0), Point((i)*(WIDTH/7), HEIGHT))
+        grid.append(line)
+        gridcoords.append(int((i)*(WIDTH/7)))
+    print(gridcoords)
+    #draw road lines, first parameter is how many to draw. 3 means 4 rows, and that scales in the same manner
+    rLinesNumber = numLanes-1
+    rlines = drawLines(rLinesNumber, win, roadBuff)
+
+    #draw point counter
+    pCounter = Text(Point(WIDTH/2, roadBuff/2), "Points: ")
+    pCounter.setSize(24)
+    pCounter.setStyle("bold")
+    pCounter.draw(win)
+    
+    #create a 2D array that acts as our MDP with rLinesNumber+1 rows 
+    columns = 7
+    MDP = [['e' for x in range(columns)] for y in range (numLanes)] 
+    #print (MDP)
+
+    #calculate lane width
+    laneWidth = rlines[0].getCenter().getY() - roadBuff
+
+    #create Arrays to hold coordinates for car locations
+    xcoords = []
+    for i in range(7):
+        x = int(((i*2)+1)*(WIDTH/14))
+        xcoords.append(x-(x%5))
+    xmov = (xcoords[1] - xcoords[0])/10
+    ycoords = []
+    for i in range(numLanes):
+        y = int((roadBuff+(laneWidth/2))+(i*laneWidth))
+        ycoords.append(y-(y%5))
+    ymov = (ycoords[1] - ycoords[0])/10
+    
+    #resize player car to fit in lane
+    image = img.open("car.png")
+    newHeight = int(laneWidth*0.7)
+    hpercent = (newHeight / float(image.size[1]))
+    wsize = int((float(image.size[0])*float(hpercent)))
+    image = image.resize((wsize, newHeight), img.ANTIALIAS)
+    image.save('car.png')
+    
+    #draw car in the middle lane
+    carX = 3
+    carY = int(numLanes/2)
+    MDP[carY][carX] = 'p'
+    playCar = Image(Point(xcoords[carX], ycoords[carY]), "car.png")
+    playCar.draw(win)
+
+    #resize oil slick to fit in line
+    image = img.open("oilSlick.png")
+    newHeight = int(laneWidth*0.7)
+    hpercent = (newHeight / float(image.size[1]))
+    wsize = int((float(image.size[0])*float(hpercent)))
+    image = image.resize((wsize, newHeight), img.ANTIALIAS)
+    image.save('oilSlick.png')
+
+    #draw oil in the middle lane
+    oilX = 6
+    oilY = int(numLanes/2)
+    MDP[oilY][oilX] = 'o'
+    oil = Image(Point(xcoords[oilX], ycoords[oilY]), "oilSlick.png")
+    oil.draw(win)
+    if numOil > 1:
+        oil2X = 3
+        oil2Y = 0
+        MDP[oil2Y][oil2X] = 'o'
+        oil2 = Image(Point(xcoords[oil2X], ycoords[oil2Y]), "oilSlick.png")
+        oil2.draw(win)
+
+    #resize enemy car to fit in lane
+    image = img.open("badCar.png")
+    newHeight = int(laneWidth*0.7)
+    hpercent = (newHeight / float(image.size[1]))
+    wsize = int((float(image.size[0])*float(hpercent)))
+    image = image.resize((wsize, newHeight), img.ANTIALIAS)
+    image.save('badCar.png')
+
+    #draw enemy car in the middle lane
+    bcarSp = 5
+    bcarX = 0
+    bcarY = int(numLanes/2)+1
+    MDP[bcarY][bcarX] = 'b'
+    badCar = Image(Point(xcoords[bcarX], ycoords[bcarY]), "badCar.png")
+    badCar.draw(win)
+    if numCars > 1:
+        bcar2Sp = bcarSp+2
+        bcar2X = 0
+        bcar2Y = numLanes-1
+        MDP[bcar2Y][bcar2X] = 'b'
+        badCar2 = Image(Point(xcoords[bcar2X], ycoords[bcar2Y]), "badCar.png")
+        badCar2.draw(win)
+    printMDP()
+
+    #loop until done
+    while not done:
+        #if q was pressed, toggle the grid drawing
+        if qp:
+            if not drawn:
+                for g in grid:
+                    g.draw(win)
+            else:
+                for g in grid:
+                    g.undraw()
+            qp = False
+            drawn = not drawn
+        if move:
+            x = playCar.getAnchor().getX()
+            y = playCar.getAnchor().getY()
+            if x < xcoords[carX]:
+                xmov = (xcoords[carX]-xcoords[carX-1])/5
+                playCar.move(xmov,0)
+            if x > xcoords[carX]:
+                xmov = (xcoords[carX+1]-xcoords[carX])/5
+                playCar.move(-xmov,0)
+            if y < ycoords[carY]:
+                ymov = (ycoords[carY]-ycoords[carY-1])/5
+                playCar.move(0,ymov)
+            if y > ycoords[carY]:
+                ymov = (ycoords[carY+1]-ycoords[carY])/5
+                playCar.move(0,-ymov)
+            if x == xcoords[carX] and y == ycoords[carY]:
+                move = False
+        #calculate obstacle edges
+        moveObst(speed)
+        if bcarY == carY:
+            badCarR = badCar.getAnchor().getX()+(badCar.getWidth()/2)
+            badCarL = badCar.getAnchor().getX()-(badCar.getWidth()/2)
+            cx = playCar.getAnchor().getX()
+            cxr = cx + (playCar.getWidth()/2)
+            cxl = cx - (playCar.getWidth()/2)
+            collision = (cxr >= badCarL) and (cxl <= badCarR)
+            if collision:
+                print("you lose")
+                end_game()
+        if numCars > 1 and bcar2Y == carY:
+            badCarR = badCar2.getAnchor().getX()+(badCar2.getWidth()/2)
+            badCarL = badCar2.getAnchor().getX()-(badCar2.getWidth()/2)
+            cx = playCar.getAnchor().getX()
+            cxr = cx + (playCar.getWidth()/2)
+            cxl = cx - (playCar.getWidth()/2)
+            collision = (cxr >= badCarL) and (cxl <= badCarR)
+            if collision:
+                print("you lose")
+                end_game()
+        if oilY == carY:
+            oilR = oil.getAnchor().getX()+(oil.getWidth()/2)
+            oilL = oil.getAnchor().getX()-(oil.getWidth()/2)
+            cx = playCar.getAnchor().getX()
+            cxr = cx + (playCar.getWidth()/2)
+            cxl = cx - (playCar.getWidth()/2)
+            collision = (cxr >= oilL) and (cxl <= oilR)
+            if collision:
+                coin = random.randint(1, 2)
+                if coin == 1:
+                    moveCar("down")
+                else:
+                    moveCar("up")
+        if numOil > 1 and oil2Y == carY:
+            oilR = oil2.getAnchor().getX()+(oil2.getWidth()/2)
+            oilL = oil2.getAnchor().getX()-(oil2.getWidth()/2)
+            cx = playCar.getAnchor().getX()
+            cxr = cx + (playCar.getWidth()/2)
+            cxl = cx - (playCar.getWidth()/2)
+            collision = (cxr >= oilL) and (cxl <= oilR)
+            if collision:
+                coin = random.randint(1, 2)
+                if coin == 1:
+                    moveCar("down")
+                else:
+                    moveCar("up")
+
+        #move the road lines
+        for i in rlines:
+            rmove(i, -speed, 0)
+        #increase speed by acceleration
+        speed += acceleration
+
+        #increase points, plan to scale with speed later
+        points += 1
+        pCounter.setText("Points: " + str(points))
+        #if you get enough points, you win
+        if points >= p2Win:
+            done = True
+            print("you win!")
+        #print(points)
+    end_game()
+
+#stop game method
+def end_game():
+    global listener, win, points
+    print("points:" + str(points))
+    listener.stop()
+    win.close()
+    sys.exit()
+
+def printMDP():
+    global MDP
+    for row in MDP:
+        print(row)
+    print()
+
+def moveObst(speed):
+    global WIDTH, xcoords, ycoords
+    global numCars, numOil
+    global badCar, bcarX, bcarY, bcarSp
+    global badCar2, bcar2X, bcar2Y, bcar2Sp
+    global oil, oilX, oilY
+    global oil2, oil2X, oil2Y
+    global gridcoords
+    
+    oilR = oil.getAnchor().getX()+(oil.getWidth()/2)
+    if oilR <= 0:
+        MDP[oilY][oilX] = 'e'
+        oilY = random.randint(0,numLanes-1)
+        ydiff = ycoords[oilY]-oil.getAnchor().getY()
+        oil.move(WIDTH+oil.getWidth(), ydiff)
+        oilX = 6
+        MDP[oilY][oilX] = 'o'
+        #printMDP()
+    else:
+        oil.move(-speed, 0)
+        if oilX != 0 and oil.getAnchor().getX() < gridcoords[oilX]:
+            MDP[oilY][oilX] = 'e'
+            oilX -= 1
+            MDP[oilY][oilX] = 'o'
+            #printMDP()
+
+    if numOil > 1:
+        oilR = oil2.getAnchor().getX()+(oil2.getWidth()/2)
+        if oilR <= 0:
+            MDP[oil2Y][oil2X] = 'e'
+            oil2Y = random.randint(0,numLanes-1)
+            ydiff = ycoords[oil2Y]-oil2.getAnchor().getY()
+            oil2.move(WIDTH+oil2.getWidth(), ydiff)
+            oil2X = 6
+            MDP[oil2Y][oil2X] = 'o'
+            #printMDP()
+        else:
+            oil2.move(-speed, 0)
+            if oil2X != 0 and oil2.getAnchor().getX() < gridcoords[oil2X]:
+                MDP[oil2Y][oil2X] = 'e'
+                oil2X -= 1
+                MDP[oil2Y][oil2X] = 'o'
+                #printMDP()
+            
+    badCarL = badCar.getAnchor().getX()-(badCar.getWidth()/2)
+    if badCarL >= WIDTH:
+        MDP[bcarY][bcarX] = 'e'
+        bcarY = random.randint(0,numLanes-1)
+        ydiff = ycoords[bcarY]-badCar.getAnchor().getY()
+        badCar.move(-WIDTH-badCar.getWidth(), ydiff)
+        bcarX = 0
+        MDP[bcarY][bcarX] = 'b'
+        bcarSp += 1
+        #printMDP()
+    else:
+        badCar.move(bcarSp, 0)
+        if bcarX != 6 and badCar.getAnchor().getX() > gridcoords[bcarX]:
+            MDP[bcarY][bcarX] = 'e'
+            bcarX += 1
+            MDP[bcarY][bcarX] = 'b'
+            #printMDP()
+
+    if numCars > 1:
+        badCarL = badCar2.getAnchor().getX()-(badCar2.getWidth()/2)
+        if badCarL >= WIDTH:
+            MDP[bcar2Y][bcar2X] = 'e'
+            bcar2Y = random.randint(0,numLanes-1)
+            if bcar2Y == bcarY and bcarY != 0:
+                if bcarY != 0:
+                    bcar2Y = bcarY-1
+                else:
+                    bcar2Y = bcarY+1
+            ydiff = ycoords[bcar2Y]-badCar2.getAnchor().getY()
+            badCar2.move(-WIDTH-badCar2.getWidth(), ydiff)
+            bcar2X = 0
+            MDP[bcar2Y][bcar2X] = 'b'
+            bcar2Sp += 1
+            printMDP()
+        else:
+            badCar2.move(bcar2Sp, 0)
+            if bcar2X != 6 and badCar2.getAnchor().getX() > gridcoords[bcar2X]:
+                MDP[bcar2Y][bcar2X] = 'e'
+                bcar2X += 1
+                MDP[bcar2Y][bcar2X] = 'b'
+                printMDP()
+
+def moveCar(drxn):
+    global playCar, carX, carY, MDP, move
+    MDP[carY][carX] = 'e'
+    if drxn == "down" and carY < (len(MDP)-1):
+        carY += 1
+        move = True
+    elif drxn == "up" and carY > 0:
+        carY -= 1
+        move = True
+    elif drxn == "left" and carX > 0:
+        carX -= 1
+        move = True
+    elif drxn == "right" and carX < (len(MDP[0])-1):
+        carX += 1
+        move = True
+    MDP[carY][carX] = 'p'
+    #printMDP()
+
+#keyboard listener method
+def on_press(key):
+    global done, drawn, qp, MDP
+    if key == Key.esc:  #press esc to quit
+        done = True
+    elif key.char == 'q': #press q to toggle the grid
+        qp = True
+    elif key.char == 's':
+        moveCar("down")
+    elif key.char == 'w':
+        moveCar("up")
+    elif key.char == 'a':
+        moveCar("left")
+    elif key.char == 'd':
+        moveCar("right")
+
+#road line movement method
+#Parameters: Rectangle graphics object, int, int
+def rmove(rect, xm, ym):
+    if(rect.getP2().getX() <= -50):
+        rect.move(WIDTH+100, ym)
+    else:
+        rect.move(xm, ym)
+
+#create rectangle method
+#Parameters: tuple (x,y), tuple (width,height)
+def make_rect(center,size):
+    return (Point(center[0]-(size[0]/2), center[1]-(size[1]/2)),Point(center[0]+(size[0]/2), center[1]+(size[1]/2)))
+
+#draw road method
+#Parameters: graphics window, int
+def drawRoad(win, roadBuff):
+    upEdge = roadBuff
+    downEdge = HEIGHT - roadBuff
+    
+    road = Rectangle(Point(WIDTH,downEdge), Point(-1,upEdge))
+    road.setOutline("black")
+    road.setFill("grey")
+    road.draw(win)
+    sepHeight = HEIGHT/10
+
+#draw road lines method
+#Parameters: int, graphics window, int
+def drawLines(numLines, win, buff):
+    line = []
+    numTix = int((WIDTH/2)/50.0)
+    
+    roadWidBuff = (HEIGHT-(buff*2))/(numLines+1)
+
+    for k in range(numLines):
+        for i in range(numTix+1):
+            yr = make_rect((i*(100.0)+(50.0), buff+(k+1)*roadWidBuff),(50.0, 50.0/(2*numLines)))
+            yrect = Rectangle(yr[0], yr[1])
+            yrect.setOutline("black")
+            yrect.setFill("yellow")
+            line.append(yrect)
+
+    for i in line:
+        i.draw(win)
+    return line
+
+main()

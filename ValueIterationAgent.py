d90c3c0ebb2b13f110d9dd40d69c7d153aee0693
MattKey <m4ttk3y@gmail.com>
2018-12-01 14:44:03 -0800

    update 2:43 pm

--- ValueIterationAgent.py
+++ ValueIterationAgent.py
@@ -1,3 +1,4 @@
+import MDPReader as mdpr
 class ValueIterationAgent:
 
     def __init__(self, mdp, iterations):
@@ -5,7 +6,18 @@ class ValueIterationAgent:
         self.discount = 0.9
         self.iterations = iterations
 
-        
+        self.values = {}
+
+        for i in range(0, self.iterations):
+            newVals = {}
+            mdpReader = mdpr.MDPReader()
+            for s in mdpReader.getStates(self.mdp):
+                bestActV = -999999
+                actions = mdpReader.getLegalActions(self.mdp, s)
+                if actions is None:
+                    newVals[s] = 0
+                for a in actions:
+                    actSum = self#--CONTINUE HERE
 
     def dothing(self):
         for i in range(self.iterations):
